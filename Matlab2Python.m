2f6e3366d7d74fc783af29d4d955b9419a0134d4
hundredball <chh008@eng.ucsd.edu>
2020-05-28 00:47:49 -0700

    Record all the channels

--- Matlab2Python.m
+++ Matlab2Python.m
@@ -4,56 +4,56 @@ clear all; clc; close all;
 T_data = readtable('./Data_Matlab/data_list.xlsx');
 data_list = table2array(T_data);
 
-% Load default channel coordinates
-default_loc = readlocs('./Channel_coordinate/Standard-10-20-Cap81.locs');
+% % Load default channel coordinates
+% default_loc = readlocs('./Channel_coordinate/Standard-10-20-Cap81.locs');
 
 % Load channel of interest
-T = readtable('./Channel_coordinate/Channel_location_angle.xlsx');
-labels_interest = table2array(T(1:end, 1));  % labels of interest
+% T = readtable('./Channel_coordinate/Channel_location_angle.xlsx');
+% labels_interest = table2array(T(1:end, 1));  % labels of interest
 
 for i_data = 1:size(data_list, 1)
     % Load data
     fileName = data_list{i_data};
     EEG = pop_loadset(['./Data_Matlab/' fileName '.set']);
 
-    % Get coordinate of channel in EEG
-    radius_EEG = cell2mat({EEG.chanlocs.radius});
-    theta_EEG = mod(cell2mat({EEG.chanlocs.theta})+360, 360)/360;
-
-    % Find matched channels by sph_theta, sph_phi
-    for i = 1:size(labels_interest, 1)
-        label = labels_interest{i};
-        indice_in_default = find(ismember({default_loc.labels}, {label}));
-        indice_in_EEG = find(ismember({EEG.chanlocs.labels}, {label}));
-
-        % Remove repetitive label first
-        if ~isempty(indice_in_EEG)
-            %fprintf(['[X]' EEG.chanlocs(indice_in_EEG).labels '->XX\n']);
-            EEG.chanlocs(indice_in_EEG).labels = 'XX';
-        end
-
-        if ~isempty(indice_in_default)
-            radius = default_loc(indice_in_default).radius;
-            theta = mod(default_loc(indice_in_default).theta+360, 360)/360;
-
-            % Choose channel with the smallest difference of phi and theta
-            [value, index] = min( abs(radius_EEG-radius)+abs(theta_EEG-theta));
-            radius_EEG(index) = nan;
-            theta_EEG(index) = nan;
-
-
-            %fprintf(['[O]' EEG.chanlocs(index).labels '->' label '\n']);
-            EEG.chanlocs(index).labels = label;
-        end
-    end
-
-    % Reserve channels of interest
-    EEG = pop_select(EEG, 'channel', labels_interest);
+%     % Get coordinate of channel in EEG
+%     radius_EEG = cell2mat({EEG.chanlocs.radius});
+%     theta_EEG = mod(cell2mat({EEG.chanlocs.theta})+360, 360)/360;
+% 
+%     % Find matched channels by sph_theta, sph_phi
+%     for i = 1:size(labels_interest, 1)
+%         label = labels_interest{i};
+%         indice_in_default = find(ismember({default_loc.labels}, {label}));
+%         indice_in_EEG = find(ismember({EEG.chanlocs.labels}, {label}));
+% 
+%         % Remove repetitive label first
+%         if ~isempty(indice_in_EEG)
+%             %fprintf(['[X]' EEG.chanlocs(indice_in_EEG).labels '->XX\n']);
+%             EEG.chanlocs(indice_in_EEG).labels = 'XX';
+%         end
+% 
+%         if ~isempty(indice_in_default)
+%             radius = default_loc(indice_in_default).radius;
+%             theta = mod(default_loc(indice_in_default).theta+360, 360)/360;
+% 
+%             % Choose channel with the smallest difference of phi and theta
+%             [value, index] = min( abs(radius_EEG-radius)+abs(theta_EEG-theta));
+%             radius_EEG(index) = nan;
+%             theta_EEG(index) = nan;
+% 
+% 
+%             %fprintf(['[O]' EEG.chanlocs(index).labels '->' label '\n']);
+%             EEG.chanlocs(index).labels = label;
+%         end
+%     end
+% 
+%     % Reserve channels of interest
+%     EEG = pop_select(EEG, 'channel', labels_interest);
 
     % Save channel data, events and location names
     data = EEG.data;
     event = {EEG.event.latency;EEG.event.type}';
-    chanloc_labels = {EEG.chanlocs.labels}';
-    save(['./Data_Python/' fileName(1:6) '.mat'], 'data', 'event', 'chanloc_labels');
-    fprintf([int2str(i_data) '. Save ' fileName(1:6) '.mat'])
+    chanlocs = EEG.chanlocs;
+    save(['./Data_Python/' fileName(1:6) '.mat'], 'data', 'event', 'chanlocs');
+    fprintf([int2str(i_data) '. Save ' fileName(1:6) '.mat\n'])
 end
--- dataloader.py
+++ dataloader.py
@@ -10,20 +10,19 @@ import numpy as np
 import pandas as pd
 import scipy.io as sio
 
-def extract_data(data, events, channels, diff_type):
+def extract_data(data, events, i_file, diff_type):
     '''
     Extract baseline data and solution time
 
     Parameters
     ----------
-    data : numpy 3D array (i, j, k)
-        i : example
+    data : numpy 2D array (j, k)
         j : channel
         k : sample
     events : numpy 2D array
         event time and type
-    channels : numpy 2D array
-        channel names
+    i_file : int
+        index of the file in data_list.csv
     diff_type : str
         difficulty of interest (1,2,3)
 
@@ -40,7 +39,7 @@ def extract_data(data, events, channels, diff_type):
     assert isinstance(diff_type, str)
     
     sampling_rate = 256
-    num_channel = channels.shape[0]
+    num_channel = data.shape[0]
     num_epoch = sum([1 if x[1]==diff_type else 0 for x in events])
     X = np.zeros((num_epoch, num_channel, sampling_rate*2))
     Y = np.zeros(num_epoch)
@@ -56,7 +55,9 @@ def extract_data(data, events, channels, diff_type):
             Y[iter_event] = int(events[i+1,0] - event_time)/sampling_rate
             iter_event += 1
             
-    return X, Y
+    C = np.zeros(X.shape[0]) + i_file
+    
+    return X, Y, C
     
 
 def read_data(diff_type):
@@ -76,6 +77,8 @@ def read_data(diff_type):
         k : sample
     Y : numpy 1D array
         solution latency of example
+    C : numpy 1D array
+        channel information of example
 
     '''
     
@@ -86,76 +89,106 @@ def read_data(diff_type):
     df_names = pd.read_csv('./Data_Matlab/data_list.csv')
     data_names = [x[0:6] for x in df_names.values.flatten()]
     
-    # Get channel order
-    df_channel = pd.read_csv('./Channel_coordinate/Channel_location_angle.csv')
-    channel_order = df_channel['Channel'].to_numpy()
     
     # Iterate over each files
-    X_list_diff_date = [[] for x in range(3)]
-    Y_list_diff_date = [[] for x in range(3)]
+    X_list_diff_date = [[] for x in range(3)]   # Data
+    Y_list_diff_date = [[] for x in range(3)]   # Labels
+    C_list_diff_date = [[] for x in range(3)]   # Channel info
     
-    for fileName in data_names:
+    for i_file, fileName in enumerate(data_names):
         EEG = sio.loadmat('./Data_Python/%s.mat'%(fileName))
         data = EEG['data']
         events = EEG['event']
-        channels = EEG['chanloc_labels'].flatten()
+        chanlocs = EEG['chanlocs']
+        if data.shape[0]!=128:
+            print('%s has only %d channels'%(fileName, data.shape[0]))
+            continue
         
-        # Set channel order of the data
-        reorder = [np.where(channels == x)[0][0] for x in channel_order]
-        data = data[reorder,:]
+        # Extract channel label, theta, arc length
+        num_channel = chanlocs.shape[1]
+        chan_label, chan_theta, chan_arc_length = [], [], []
         
-        X1, Y1 = extract_data(data, events, channels, '1')
-        X2, Y2 = extract_data(data, events, channels, '2')
-        X3, Y3 = extract_data(data, events, channels, '3')
+        for i in range(num_channel):
+            chan_label.append(chanlocs[0,i][0][0])
+            chan_theta.append(chanlocs[0,i][1][0,0])
+            chan_arc_length.append(chanlocs[0,i][2][0,0])
+            
+        # create dataframe for them
+        '''
+        channels : pandas DataFrame (num_channel, 3)
+            label : name of channels
+            theta : 0 toward naison, positive for right hemisphere
+            arc_length : arc_length of each channel, diameter of circle is 1
+        '''
+        d = {'label':chan_label, 'theta':chan_theta, 'arc_length':chan_arc_length}
+        channels = pd.DataFrame(data=d)
+        channels.to_csv('./Channel_coordinate/%s_channels.csv'%(fileName))
+        
+        X1, Y1, C1 = extract_data(data, events, i_file, '1')
+        X2, Y2, C2 = extract_data(data, events, i_file, '2')
+        X3, Y3, C3 = extract_data(data, events, i_file, '3')
                 
-        X_list_diff_date[0].append(X1)
-        Y_list_diff_date[0].append(Y1)
-        X_list_diff_date[1].append(X2)
-        Y_list_diff_date[1].append(Y2)
-        X_list_diff_date[2].append(X3)
-        Y_list_diff_date[2].append(Y3)
+        X_list_diff_date[0].append(X1.copy())
+        Y_list_diff_date[0].append(Y1.copy())
+        C_list_diff_date[0].append(C1.copy())
+        
+        X_list_diff_date[1].append(X2.copy())
+        Y_list_diff_date[1].append(Y2.copy())
+        C_list_diff_date[1].append(C2.copy())
+        
+        X_list_diff_date[2].append(X3.copy())
+        Y_list_diff_date[2].append(Y3.copy())
+        C_list_diff_date[2].append(C3.copy())
         
         
     # Concatenate over dates
     X_list_diff = []
     Y_list_diff = []
+    C_list_diff = []
     for i in range(len(X_list_diff_date)):
         X_list_date = X_list_diff_date[i]
         Y_list_date = Y_list_diff_date[i]
+        C_list_date = C_list_diff_date[i]
         
         for j in range(len(X_list_date)):
             if j == 0:
                 X = X_list_date[j]
                 Y = Y_list_date[j]
+                C = C_list_date[j]
             else:
                 X = np.concatenate((X,X_list_date[j]), axis=0)
                 Y = np.concatenate((Y,Y_list_date[j]))
+                C = np.concatenate((C,C_list_date[j]))
                 
         X_list_diff.append(X.copy())
         Y_list_diff.append(Y.copy())
+        C_list_diff.append(C.copy())
         
     print('Event 1 X shape: ', X_list_diff[0].shape)
     print('Event 2 X shape: ', X_list_diff[1].shape)
     print('Event 3 X shape: ', X_list_diff[2].shape)
+    
     # Concatenate over difficulties
     for i in range(len(diff_type)):
         index_list = diff_type[i]-1
         if i == 0:
             X = X_list_diff[index_list]
             Y = Y_list_diff[index_list]
+            C = C_list_diff[index_list]
         else:
             X = np.concatenate((X, X_list_diff[index_list]), axis=0)
             Y = np.concatenate((Y, Y_list_diff[index_list]))
+            C = np.concatenate((C, C_list_diff[index_list]))
             
     print('Combined X shape: ', X.shape)
     
-    return X, Y, channel_order
+    return X, Y, C
 
 
 
 if __name__ == '__main__':
     
-    X, Y, channel_order = read_data([1,2,3])
+    X, Y, C = read_data([1,2,3])
     print('X shape: ', X.shape)
-    print(channel_order)
+    print(C)
     
